03a3977857406ddd86aad8713e4c1840d5a4f13f
LilyN11 <lilynguyen1011@gmail.com>
2024-01-25 13:46:41 -0500

    Comments

--- Pset0/app.py
+++ Pset0/app.py
@@ -23,6 +23,7 @@ def home():
 
 @app.route("/add", methods=["POST"])
 def add():
+    #Concept: User Input Box
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
@@ -32,17 +33,19 @@ def add():
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
+    # Concept: Status Indicator 
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
 
+#Concept: Deletion
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
-    
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
